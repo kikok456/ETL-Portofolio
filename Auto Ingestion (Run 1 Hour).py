bb2739b2e5bb0639aeaba216a8af237bc3b14e2a
Virqy Apriyadi Jihad <86461430+kikok456@users.noreply.github.com>
2023-03-29 09:46:11 +0700

    commit4

--- Auto Ingestion (Run 1 Hour).py
+++ Auto Ingestion (Run 1 Hour).py
@@ -14,7 +14,6 @@ import numpy as np
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-
 spark = SparkSession \
         .builder \
         .appName("Update zabbix_tb_history_uint") \
